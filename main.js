585a3fc169d8636924beb339ebd8cbcab99efc61
econtreras <econtreras@anticipa.cl>
2023-04-17 21:34:39 -0400

    first Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let current_star_level;
+const allstars = document.querySelectorAll('.star');
+allstars.forEach((star,i)=>{
+    star.onclick = function(){
+        current_star_level = i+1;
+       
+        allstars.forEach((star,j)=>{
+            if(current_star_level>= j+1){
+                star.innerHTML = '&#9733';
+            }else{
+                star.innerHTML = '&#9734';
+            }
+        })
+    }
+});
+
+
